b490c1bd864725b9be91b5724da0ee382d34cb3a
@_Hacky_Coder <jignesh.vision.17@gmail.com>
2024-04-07 19:13:23 +0530

    New File Uploaded

---
+++ JS_Variables/index.js
@@ -0,0 +1,21 @@
+var x = 10, y = 50, z = x + y;
+
+let a = 15.695, 
+ b = 100,
+ c = a * b ;
+{
+    let a = 20;
+    b = 23;
+    c = a / b * 2 +50*2.1;
+}
+
+var value = 50;
+function d() {
+    alert(window.value);
+}
+
+
+
+const $_i = "hello world";
+const _ii = "my first programm output is ";
+const iii = _ii + $_i;
