6ae38804a37db1b49429cfca1e5708da8caca528
@_Hacky_Coder <jignesh.vision.17@gmail.com>
2024-04-08 15:56:41 +0530

    conversion Operations

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,29 @@
+let score = "jignesh";
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+let valueInNumber = Number(score) // guranteed that score is converted into numbers
+
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber); // NaN - Not a Number it is a also special type
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false =>0
+
+let isLoggedIn = "jignesh"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+
+//console.log(booleanIsLoggedIn); // Boolean
+
+// 1 => true; 0 => false
+// "" => false;
+// "jignesh" => true
+
+let someNumber = 44
+
+let stringNumber = String(someNumber);
+console.log(stringNumber); 
+console.log(typeof stringNumber);
