ab1b6aaf37186ef8e6ccc68c7338baf146ba99f1
@_Hacky_Coder <jignesh.vision.17@gmail.com>
2024-04-08 14:29:03 +0530

    New File Updated

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,31 @@
+"use strict"; // treat all JS Code as newer version
+
+// this is a pop up message box with the text Hello inside of it.  
+//alert("Hello") 
+/*
+also we are using node JS, not browser 
+there is deferent method and syntax for pop up message box in node JS
+*/
+console.log(30
+    +
+    35) // code readability should be high
+
+console.log("heelo")
+
+let name = "jignesh"
+let age = 18
+let isLoggedIn = false
+let state;
+
+// Premitive Data Types
+//01 - Number = > 2 to power 53
+//02 - bigint 
+//03 - String ="Jignesh"
+//04 - boolean= true or false
+//05 - null => nothing (Standalone value) , 
+//06 - undefined => garbage value
+//07 - symbol => unique
+
+//08 - object 
+console.log(typeof undefined); //undefined is self data type 
+console.log(typeof null); //null is a object
