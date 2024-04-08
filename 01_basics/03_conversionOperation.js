dfeb4351b8e28b5cd6d1b62f54ed0dc4614e8589
@_Hacky_Coder <jignesh.vision.17@gmail.com>
2024-04-08 16:43:54 +0530

    Conversion operations and basic arithmetic/string operations added
    This diff adds conversion operations such as Boolean(), String(), negative value conversion, arithmetic operations, string concatenation, and type conversion with boolean and empty string. Comments for further execution are also included. No changes in existing conversion operation of String(someNumber).

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -25,5 +25,75 @@ let booleanIsLoggedIn = Boolean(isLoggedIn);
 let someNumber = 44
 
 let stringNumber = String(someNumber);
-console.log(stringNumber); 
-console.log(typeof stringNumber);
+//console.log(stringNumber); 
+//console.log(typeof stringNumber);
+
+//********************* Operations *******************/
+
+let value = 3
+let negValue = -value // negative value conversion
+// console.log(negValue);
+
+/*
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**2);
+console.log(2/3);
+console.log(2%2);
+*/
+
+let str1 = "hello"
+let str2 = " Jignesh"
+
+let str3 = str1 + str2
+
+/*
+console.log(str3);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2" );
+*/
+
+//console.log( +true );
+//console.log( +"" );
+
+let num1, num2, num3;
+
+num2 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+++gameCounter;
+//console.log(gameCounter); 
+
+
+let a = 1;
+let x = 1 + (a *= 2);
+//console.log(a,x);
+
+/* 
+"" + 1 + 0 = "10" // (1)
+"" - 1 + 0 = -1 // (2)
+true + false = 1
+6 / "3" = 2
+"2" * "3" = 6
+4 + 5 + "px" = "9px"
+"$" + 4 + 5 = "$45"
+"4" - 2 = 2
+"4px" - 2 = NaN
+"  -9  " + 5 = "  -9  5" // (3)
+"  -9  " - 5 = -14 // (4)
+null + 1 = 1 // (5)
+undefined + 1 = NaN // (6)
+" \t \n" - 2 = -2 // (7)
+*/
+
+//01 - The addition with a string "" + 1 converts 1 to a string: "" + 1 = "1", and then we have "1" + 0, the same rule is applied.
+//02 - The subtraction - (like most math operations) only works with numbers, it converts an empty string "" to 0.
+//03 - The addition with a string appends the number 5 to the string.
+//04 - The subtraction always converts to numbers, so it makes " -9 " a number -9 (ignoring spaces around it).
+//05 - null becomes 0 after the numeric conversion.
+//06 - undefined becomes NaN after the numeric conversion.
+//07 - Space characters are trimmed off string start and end when a string is converted to a number. Here the whole string consists of space characters, such as \t, \n and a “regular” space between them. So, similarly to an empty string, it becomes 0.
