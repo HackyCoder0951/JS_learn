c5a069f3fc9162f5f88a62d5ce4555f87bf660b8
@_Hacky_Coder <jignesh.vision.17@gmail.com>
2024-04-08 15:26:23 +0530

    New File Updated

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -1,3 +1,14 @@
+/*JS and Camel Case
+01 - Pascal Case :- We can create variable likes SmartWatch,MobileDevie,WebDriver etc. 
+    It Represent the upper camel case string.
+
+02 - Lower Camel Case :- JS allows to use variable names and expression names likes
+    smartWatch, mobileDevice, webDriver etc.
+
+03 - Under Score :-  we can use underscore while declaring variables likes smar_watch, mobile_device,
+    web_driver etc.
+*/
+
 const accountId = 245005;  // constant value reference
 let accountEmail = "jignesh.vision@google.com" // variable declaration using let
 var accountPassword = "12345" // variable declaration using var that is not preferred 
