de6825f70ad24af0cde0c912b7820d5cae7ab89e
@_Hacky_Coder <jignesh.vision.17@gmail.com>
2024-04-09 15:53:28 +0530

    execution context

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,16 @@
-console.log("Jignesh Ameta")
+/*
+
+Execution context
+
+------------------------------------------
+Memory Component| Code Component
+(Variable Env)  | (Thread of Execution)
+------------------------------------------
+key : value     | * ....................
+a : 10          | * ....................
+fn : {}         | * ....................
+
+JS is a synchronous singl-threaded language
+
+
+*/
