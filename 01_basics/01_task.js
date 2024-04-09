1f828749250f9a01b7dcd082379a37c1f6a2a51c
@_Hacky_Coder <jignesh.vision.17@gmail.com>
2024-04-09 19:15:04 +0530

    task by bhasker

---
+++ 01_basics/01_task.js
@@ -0,0 +1,26 @@
+/* 
+Create a function called "beautifyString" 
+
+which accept only one argument.
+
+and convert that string into desired format.
+*/
+/*
+:-> The first character of every word in a string should be capital case and remaining in lower case.
+:-> Every word of the string should be separated by "-" (dash) symbol.
+:-> You must need to remove every numbers from the string.
+:-> If string contain only a single word then do not add "-" (dash) symbol.
+*/
+/* Test Cases */
+/* Input */
+    // beautifyString("hello world")
+    // beautifyString("This is soMe text")
+    // beautifyString("the MAN 90 is lost HIS power")
+    // beautifyString("hi")
+
+/* Output */
+    // Hello-World
+    // This-Is-Some-Text
+    // The-Man-Is-Lost-His-Power
+    // Hi
+    
