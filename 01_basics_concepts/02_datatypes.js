b69f5e4a46e6ff9caa2f7c9b0569f7732cd5877b
@_Hacky_Coder <jignesh.vision.17@gmail.com>
2024-06-16 07:02:15 +0000

    Update Files

--- 01_basics_concepts/02_datatypes.js
+++ 01_basics_concepts/02_datatypes.js
@@ -11,9 +11,20 @@ there is deferent method and syntax for pop up message box in node JS
 // Here JS interprets the line break as implicit semicolon;
 // This is called "automatic Semicolon insertion".
 
-console.log(30
-    +
-    35) // code readability should be high
+//In most cases, a newline implies a semicolon. But “in most cases” does not mean “always”!
+//There are cases when a newline does not mean a semicolon. For example:
+console.log(30 +
+    1
+    + 35); // code readability should be high
+
+/*
+The code outputs 6 because JavaScript does not insert semicolons here. 
+It is intuitively obvious that if the line ends with a plus "+", then it is an “incomplete expression”, so a semicolon there would be incorrect.And in this case, that works as intended.
+
+But there are situations where JavaScript “fails” to assume a semicolon where it is really needed.
+
+Errors which occur in such cases are quite hard to find and fix.
+ */
 
 console.log("heelo")
 
