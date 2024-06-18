44c170c61c56f7374e716b3c3c3987f4cf71ea26
@_Hacky_Coder <jignesh.vision.17@gmail.com>
2024-06-18 13:41:32 +0000

    Files Updated

--- JS_Variables/test.js
+++ JS_Variables/test.js
@@ -1,11 +1,3 @@
-alert("hello");
-// result = prompt(title, [default]);
-let age = prompt('How old are you?', 100); // for asking value inputs from users
-
-alert(`you are ${age} years old!`);
-// diplayed values from users input 
-
-let isBoss = confirm("Are you the boss?");
-
-alert(isBoss);
+let name = prompt("What is your name?", "");
+alert(name);
 
